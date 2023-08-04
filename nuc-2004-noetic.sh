a1574edcfdc396431d7412a95886edd49e7a850a
Tomohiro Hayashi <tomohiro@kudan.eu>
2023-08-04 16:07:03 +0900

    Ubuntu 20.04 noetic setup

---
+++ nuc-2004-noetic.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+echo "Install Google Chrome"
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt update
+sudo apt install -y google-chrome-stable
+
+echo "Install basic tools"
+sudo apt update
+sudo apt install -y \
+    wget \
+    curl \
+    ca-certificates \
+    python3-pip \
+    bash \
+    git \
+    unzip \
+    xterm \
+    build-essential \
+    libeigen3-dev \
+    libjsoncpp-dev \
+    libspdlog-dev \
+    cmake \
+    terminator \
+    vim \
+    gnome-tweak-tool \
+    ethtool \
+    hwinfo \
+    inxi \
+    cutecom \
+    snapd \
+    intel-gpu-tools
+
+echo "Install kazam, meld"
+sudo add-apt-repository ppa:sylvain-pineau/kazam
+sudo apt install kazam
+sudo apt install -y meld python3-nautilus
+sudo add-apt-repository ppa:boamaod/nautilus-compare
+sudo apt install -y nautilus-compare
+nautilus -q
+
+echo "Install VS code"
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install -y code
+
+echo "Install foxglove"
+sudo snap install foxglove-studio
+
+echo "Install flatpak"
+sudo apt install -y flatpak gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo "Install ROS Noetic"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install -y ros-noetic-desktop-full
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+sudo apt install -y \
+    ros-noetic-rosbag \
+    ros-noetic-pcl-ros \
+    ros-noetic-rviz \
+    ros-noetic-cv-bridge \
+    ros-noetic-tf2-geometry-msgs \
+    ros-noetic-eigen-conversions \
+    ros-noetic-tf-conversions \
+    ros-noetic-xacro \
+    ros-noetic-robot-state-publisher
