249a518dad4cce324fe709ea8f724b9eae77dab4
Tomohiro Hayashi <tomohiro@kudan.eu>
2024-01-22 17:22:48 +0900

    ros-humble

--- nuc-2204-humble.sh
+++ nuc-2204-humble.sh
@@ -42,7 +42,7 @@ sudo apt install -y meld
 echo "Install VS code"
 sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
 sudo apt update
 sudo apt install -y code
 
@@ -52,20 +52,20 @@ sudo snap install foxglove-studio
 echo "Install flatpak"
 sudo apt install -y flatpak gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.cloudcompare.CloudCompare
+flatpak install flathub org.cloudcompare.CloudCompare -y
 
 echo "Install ROS2 Humble"
 sudo apt install -y software-properties-common
-sudo add-apt-repository universe
+sudo add-apt-repository universe -y
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update && sudo apt upgrade -y
 sudo apt install ros-humble-desktop -y
 sudo apt install ros-dev-tools -y
-sudo apt install -y ros-$ROS_DISTRO-pcl-*
-sudo apt install -y ros-$ROS_DISTRO-gps-msgs
-sudo apt install -y ros-$ROS_DISTRO-image-transport-plugins
+sudo apt install -y ros-humble-pcl-*
+sudo apt install -y ros-humble-gps-msgs
+sudo apt install -y ros-humble-image-transport-plugins
 sudo apt install -y build-essential python3-colcon-common-extensions python3-rosdep ros-humble-rmw-cyclonedds-cpp
 
 echo "export RCUTILS_COLORIZED_OUTPUT=1" >> ~/.bashrc
